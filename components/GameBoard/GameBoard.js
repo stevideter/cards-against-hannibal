73dbe5d3277d150833ee5267305f929058d5bf42
Stevi Deter <stevi.deter@gmail.com>
2020-04-11 17:07:53 -0700

    feat(game): fetch card deck from Cardcast

--- components/GameBoard/GameBoard.js
+++ components/GameBoard/GameBoard.js
@@ -2,18 +2,29 @@ import styles from "./GameBoard.module.css";
 import BlackCard from "../BlackCard/BlackCard";
 import WhiteCard from "../WhiteCard/WhiteCard";
 
-const GameBoard = () => (
-  <div className={styles.gameboard}>
-    <div className={styles.blackcard}>
-    <BlackCard />
-    </div>
-    <div className={styles.whitecards}>
-      <WhiteCard text="design" />
-      <WhiteCard text="murder" />
-      <WhiteCard text="fish jello" />
+const GameBoard = (props) => {
+  let whiteCards = [];
+  let fullDeck = [ ...props.whiteCards ];
+  let cardCount = props.whiteCards.length;
+  for (let i = 0; i < 10; i++) {
+    const randomIndex = Math.floor(Math.random() * cardCount);
+    const card = fullDeck.splice(randomIndex, 1)[0];
+    whiteCards.push(<WhiteCard text={card.text} key={i} id={card.id} />);
+    cardCount--;
+  }
+  const randomIndex = Math.floor(Math.random() * props.blackCards.length);
 
+  const blackCard = <BlackCard text={props.blackCards[randomIndex].text} />
+  return (
+    <div className={styles.gameboard}>
+      <div className={styles.blackcard}>
+        {blackCard }
+      </div>
+      <div className={styles.whitecards}>
+        {whiteCards}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default GameBoard;
--- pages/game/[id].js
+++ pages/game/[id].js
@@ -1,14 +1,35 @@
 import { useRouter } from 'next/router';
 import GameBoard from '../../components/GameBoard/GameBoard';
+import fetch from 'isomorphic-unfetch';
 
-const Game = () => {
+const Game = props => {
   const router = useRouter();
   const { id } = router.query;
 
   return <div>
       <p>Game: {id}</p>
-      <GameBoard />
+      <GameBoard whiteCards={props.whiteCards} blackCards={props.blackCards}/>
       </div>
 }
+const setId = 'JP8FV';
+const cardcastUrl = `https://api.cardcastgame.com/v1/decks/${setId}/cards`;
+
+Game.getInitialProps = async function() {
+  const res = await fetch(cardcastUrl);
+  const data = await res.json();
+
+  const blackCards = data.calls.map((card) => {
+    const text = card.text.join('____');
+    return { text };
+  });
+  const whiteCards = data.responses.map((card) => {
+    const text = card.text.join('');
+    return { text };
+  });
+  return {
+    blackCards,
+    whiteCards
+  };
+};
 
 export default Game

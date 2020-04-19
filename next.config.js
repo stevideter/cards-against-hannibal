851fa23948cb9f1c29f92f2d1be26553dea8f3a8
Stevi Deter <stevi.deter@gmail.com>
2020-04-18 19:39:38 -0700

    fix: ingore tests

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
     poweredByHeader: false,
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
+        return config;
+    },
 };

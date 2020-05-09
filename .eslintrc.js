8f699ffd45d7b6456d660b8f1ede7d3d9404ea66
Stevi Deter <stevi.deter@gmail.com>
2020-05-09 13:51:18 -0700

    chore: upgrade to eslint 7 (#62)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "react", "react-hooks", "prettier"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:prettier/recommended"
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
     },
-  },
-  rules: {
-    "react/react-in-jsx-scope": "off",
-  },
-  settings: {
-    react: {
-      version: "detect",
+    rules: {
+        'react/react-in-jsx-scope': 'off',
     },
-  },
-  env: {
-    node: true,
-  }
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    env: {
+        node: true,
+    },
 };

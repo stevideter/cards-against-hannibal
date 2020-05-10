6afce266c70119c4adc501274f56647432827be5
Stevi Deter <stevi.deter@gmail.com>
2020-05-09 20:29:20 -0700

    style: some css tweaks

--- src/components/BlackCard/BlackCard.tsx
+++ src/components/BlackCard/BlackCard.tsx
@@ -2,15 +2,16 @@ import styled from 'styled-components';
 
 const StyledBlackCard = styled.div`
     background: black;
+    box-shadow: rgb(102, 102, 102) 0px 2px 2px 0px;
+    box-sizing: border-box;
     color: white;
-    width: 150px;
-    height: 300px;
+    width: 10em;
+    height: 10em;
     padding: 10px;
-    font-size: xx-large;
-    font-weight: bolder;
-    border-radius: 25px;
+    border-radius: 10px;
     border: 2px solid black;
     position: relative;
+    margin: 10px;
 `;
 const StyledP = styled.p`
     margin: 0;
@@ -19,6 +20,9 @@ const StyledP = styled.p`
     left: 50%;
     transform: translate(-50%, -50%);
     text-align: center;
+    font-size: 1.3em;
+    font-family: MuseoSans, Arial, Helvetica, sans-serif;
+    text-size-adjust: 100%;
 `;
 interface BlackCardProps {
     text: string;
--- src/components/WhiteCard/WhiteCard.tsx
+++ src/components/WhiteCard/WhiteCard.tsx
@@ -4,11 +4,13 @@ import { GameBoardAction } from '../GameBoard/GameBoard';
 
 const StyledWhiteCard = styled.div`
     background: white;
+    box-shadow: rgb(102, 102, 102) 0px 2px 2px 0px;
+    box-sizing: border-box;
     color: black;
-    width: 150px;
-    height: 300px;
+    width: 10em;
+    height: 10em;
     padding: 10px;
-    border-radius: 25px;
+    border-radius: 10px;
     border: 2px solid black;
     position: relative;
     margin: 10px;
@@ -20,8 +22,9 @@ const StyledP = styled.p`
     left: 50%;
     transform: translate(-50%, -50%);
     text-align: center;
-    font-size: large;
-    font-weight: bolder;
+    font-size: 1.3em;
+    font-family: MuseoSans, Arial, Helvetica, sans-serif;
+    text-size-adjust: 100%;
 `;
 interface WhiteCardProps {
     id: string;
